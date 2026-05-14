f29a649446e52006a23b06d5c785ff8a4230d20a
caua0505 <caua.tobias012@gmail.com>
2026-05-14 15:13:27 -0300

    first commit the project

---
+++ src/main/java/com/farmacia/farmiciasystem/FarmiciaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.farmiciasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmiciaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmiciaSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farmacia/farmiciasystem/FarmiciaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.farmiciasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmiciaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
